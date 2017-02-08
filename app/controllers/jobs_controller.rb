c2b6bc489a00803a3d75e95eed2bee638050c432
QueroSerDev04 <qsd04@campuss-mini-5.home>
2017-02-08 21:52:08 -0200

    Usuário cadastra vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,4 +4,14 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.create(params.require(:job).permit(:title,
+      :location,:category,:company,:description))
+    redirect_to @job
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
-  resources :jobs, only: [:show]
+  resources :jobs, only: [:show, :new, :create]
 
 end
