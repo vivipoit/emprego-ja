5870559656f53b7c2478d5b6572e1000bbd66b53
Vivi e Jeny <jenyspa@gmail.com>
2017-02-16 19:50:50 -0200

    Usuario nao duplica categoria

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
 
   has_many :jobs
 
-  validates :name, presence: true
+  validates :name, presence: {message: 'Não foi possível criar a categoria'}
+  validates :name, uniqueness: {message: 'Não é possível duplicar categorias'}
+  
 
 end
