76cd722b7865814b5c932cfe3f4f7c924457de92
ViviPoit <vwpoit@gmail.com>
2017-02-21 00:46:59 -0300

    Home tem links para criar novos registros.

--- spec/features/visitor_visits_home_spec.rb
+++ spec/features/visitor_visits_home_spec.rb
@@ -4,5 +4,8 @@ feature 'Visitor visits Emprego Ja home page' do
   scenario 'successfully' do
     visit root_path
     expect(page).to have_content('Emprego Já')
+    expect(page).to have_content('Nova vaga')
+    expect(page).to have_content('Nova empresa')
+    expect(page).to have_content('Nova categoria')
   end
 end
